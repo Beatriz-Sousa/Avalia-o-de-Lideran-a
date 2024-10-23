fb6632fb1286d56689d256b8ba60085b494ecdc3
leandro869 <leandro.h22@hotmail.com>
2024-10-23 15:24:50 -0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  mount_devise_token_auth_for 'User', at: 'auth'
+  
+ 
+  resources :questions
+
+  resources :forms
+
+  #resources :users
+
+  get '/user', to: 'users#show'
+
+
+  post 'answer/:form_id', controller: :forms, action: :answer
+end
