270140c6c469da8bdc1eee0937c5704f4fe78420
leandro869 <leandrohdsilvalima@gmail.com>
2024-10-24 18:37:46 -0300

    Atualizando

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  mount_devise_token_auth_for 'User', at: 'auth'
+  #mount_devise_token_auth_for 'User', at: 'auth'
   
  
   resources :questions
 
   resources :forms
 
-  #resources :users
+  resources :users
 
-  get '/user', to: 'users#show'
+  #get '/user', to: 'users#show'
 
 
-  post 'answer/:form_id', controller: :forms, action: :answer
+  #post 'answer/:form_id', controller: :forms, action: :answer
 end
