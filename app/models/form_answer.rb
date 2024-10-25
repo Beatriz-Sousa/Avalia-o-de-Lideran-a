4a77f2de35abdaa7d9d6360e85eea76734ac0021
Beatriz-Sousa <beatrizssousa37@gmail.com>
2024-10-25 19:46:35 -0300

    adicionei uma nova função em form_answer

--- app/models/form_answer.rb
+++ app/models/form_answer.rb
@@ -1,4 +1,29 @@
 class FormAnswer < ApplicationRecord
-  belongs_to :form
-  belongs_to :question_user
+  has_many :question_user
+
+  def calculate_result
+    value = 0
+    question_user.each do |question_user|
+      case question_user.answer
+      when 'option_1' 
+        value += 1
+      when 'option_2'
+        value += 2
+      when 'option_3'
+        value += 3
+      else += 4
+      end
+      value
+    end
 end
+
+  def show_result
+    total = calculate_result
+    case total
+    when 18..36
+      "Liderança frágil e pouco trabalhada."
+    when 37..54
+      "Liderança em desenvolvimento."
+    else 55..72
+      "Líder de alta performance."
+  end
